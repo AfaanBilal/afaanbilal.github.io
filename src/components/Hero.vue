bf3ad6c335dc6423bc5aef22634bc3d86e932ba8
Afaan Bilal <afaanbilal@gmail.com>
2026-01-31 11:12:17 +0530

    🐛 Naming

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -4,25 +4,37 @@
             <div class="flex justify-center items-center flex-wrap">
                 <div class="py-5 mx-5 md:mx-10 text-center md:text-left">
                     <h2 class="text-4xl md:text-6xl font-bold mb-2 text-white">Afaan Bilal</h2>
-                    <h3 class="text-sm md:text-md text-gray-200 hover:text-white">Principal Software Engineer, CISO at <a class="font-bold hover:underline" href="https://www.centiment.co" target="_blank" rel="noopener">Centiment</a></h3>
-                    <h3 class="text-sm md:text-md text-gray-200 hover:text-white">Bachelor of Technology in Civil Engineering from <a class="font-bold hover:underline" href="https://www.nitsri.ac.in" target="_blank" rel="noopener">National Institute of Technology, Srinagar</a></h3>
+                    <h3 class="text-sm md:text-md text-gray-200 hover:text-white">Principal Software Engineer, CISO at
+                        <a class="font-bold hover:underline" href="https://www.centiment.co" target="_blank"
+                            rel="noopener">Centiment</a></h3>
+                    <h3 class="text-sm md:text-md text-gray-200 hover:text-white">Bachelor of Technology in Civil
+                        Engineering from <a class="font-bold hover:underline" href="https://www.nitsri.ac.in"
+                            target="_blank" rel="noopener">National Institute of Technology, Srinagar</a></h3>
                 </div>
                 <div class="py-5 md:mx-10 flex justify-center md:block">
-                    <img src="/assets/Afaan.png" alt="Afaan Bilal" class="rounded-full md:float-right w-40 md:w-auto" />
+                    <img src="/assets/afaan.png" alt="Afaan Bilal" class="rounded-full md:float-right w-40 md:w-auto" />
                 </div>
             </div>
 
             <div class="text-white text-2xl w-full text-center mb-14 flex justify-center mt-6">
-                hello <img class="w-5 mx-1" src="/assets/at.svg" alt="at" /> afaan <img class="w-2 mx-1 pt-2" src="/assets/dot.svg" alt="dot" /> dev
+                hello <img class="w-5 mx-1" src="/assets/at.svg" alt="at" /> afaan <img class="w-2 mx-1 pt-2"
+                    src="/assets/dot.svg" alt="dot" /> dev
             </div>
 
             <div class="flex justify-center mt-10">
-                <div class="pr-4 md:pr-16"><a href="https://linkedin.com/in/AfaanBilal" target="_blank" rel="noopener"><img style="width: 32px;" src="/assets/linkedin.svg" alt="linkedin" /></a></div>
-                <div class="px-4 md:px-16"><a href="https://afaan.medium.com" target="_blank" rel="noopener"><img style="width: 32px;" src="/assets/medium.svg" alt="medium" /></a></div>
-                <div class="px-4 md:px-16"><a href="https://github.com/AfaanBilal" target="_blank" rel="noopener"><img style="width: 32px;" src="/assets/github.svg" alt="github" /></a></div>
-                <div class="px-4 md:px-16"><a href="https://twitter.com/AfaanBilal" target="_blank" rel="noopener"><img style="width: 32px;" src="/assets/twitter.svg" alt="twitter" /></a></div>
-                <div class="px-4 md:px-16"><a href="https://instagram.com/AfaanBilal" target="_blank" rel="noopener"><img style="width: 32px;" src="/assets/instagram.svg" alt="instagram" /></a></div>
-                <div class="pl-4 md:pl-16"><a href="https://facebook.com/AfaanBilal" target="_blank" rel="noopener"><img style="width: 32px;" src="/assets/facebook.svg" alt="facebook" /></a></div>
+                <div class="pr-4 md:pr-16"><a href="https://linkedin.com/in/AfaanBilal" target="_blank"
+                        rel="noopener"><img style="width: 32px;" src="/assets/linkedin.svg" alt="linkedin" /></a></div>
+                <div class="px-4 md:px-16"><a href="https://afaan.medium.com" target="_blank" rel="noopener"><img
+                            style="width: 32px;" src="/assets/medium.svg" alt="medium" /></a></div>
+                <div class="px-4 md:px-16"><a href="https://github.com/AfaanBilal" target="_blank" rel="noopener"><img
+                            style="width: 32px;" src="/assets/github.svg" alt="github" /></a></div>
+                <div class="px-4 md:px-16"><a href="https://twitter.com/AfaanBilal" target="_blank" rel="noopener"><img
+                            style="width: 32px;" src="/assets/twitter.svg" alt="twitter" /></a></div>
+                <div class="px-4 md:px-16"><a href="https://instagram.com/AfaanBilal" target="_blank"
+                        rel="noopener"><img style="width: 32px;" src="/assets/instagram.svg" alt="instagram" /></a>
+                </div>
+                <div class="pl-4 md:pl-16"><a href="https://facebook.com/AfaanBilal" target="_blank" rel="noopener"><img
+                            style="width: 32px;" src="/assets/facebook.svg" alt="facebook" /></a></div>
             </div>
         </div>
     </div>
