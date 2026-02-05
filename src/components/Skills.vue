69bb76e1ed9e6106b36bb8992f17bd514c7abfee
Afaan Bilal <afaanbilal@gmail.com>
2026-02-05 18:56:06 +0530

    ✨ Add Zapier, Hubspot, Slack

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -64,7 +64,7 @@ const categories = [
         title: "Integrations & SaaS",
         color: "indigo",
         icon: "M11 4a2 2 0 114 0v1a1 1 0 001 1h3a1 1 0 011 1v3a1 1 0 01-1 1h-1a2 2 0 100 4h1a1 1 0 011 1v3a1 1 0 01-1 1h-3a1 1 0 01-1-1v-1a2 2 0 10-4 0v1a1 1 0 01-1 1H7a1 1 0 01-1-1v-3a1 1 0 00-1-1H4a2 2 0 110-4h1a1 1 0 001-1V7a1 1 0 011-1h3a1 1 0 001-1V4z",
-        skills: ["Stripe", "Plaid", "PayPal", "SendGrid", "Resend"]
+        skills: ["Stripe", "Zapier", "HubSpot", "Slack", "Plaid", "PayPal", "SendGrid", "Resend"]
     },
     {
         title: "Cloud & DevOps",
